d60d582dd7852576ff04cca959c8bcae12e2499d
muddycottage <chris@muddycottage.com>
2021-03-13 14:08:36 +0000

    now giving 403 error

--- src/main/java/com/muddycottage/muddyserver/web/SecurityConfig.java
+++ src/main/java/com/muddycottage/muddyserver/web/SecurityConfig.java
@@ -1,20 +1,24 @@
 package com.muddycottage.muddyserver.web;
 
-/*
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity ;
 
-
-//@EnableWebSecurity
-public class SecurityConfig {}
-        /*extends WebSecurityConfigurerAdapter {
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
-    protected void configure(HttpSecurity http) throws Exception {
+    public void configure(HttpSecurity http) throws Exception {
         http
                 .requiresChannel()
                 .anyRequest()
                 .requiresSecure();
+
+        http.authorizeRequests()
+                .antMatchers("/**")
+                .permitAll();
     }
 }
-*/
+
